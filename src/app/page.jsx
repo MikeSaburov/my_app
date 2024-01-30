64977ba2f18aa2915422ff88e3b2b5f6560d415f
Mikhail Saburov <saburov1985@gmail.com>
2024-01-30 23:14:24 +1100

    feature(page): добавил заголовок со стилями

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,5 +2,11 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The Feature of AI the next few years</h1>
+      </div>
+    </main>
+  );
 }
