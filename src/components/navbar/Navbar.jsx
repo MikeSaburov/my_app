463c9ce4b8d9e771b9a0a8767fdd090587292be8
Mikhail Saburov <saburov1985@gmail.com>
2024-01-30 21:55:26 +1100

    feature(navbar): добавил файл стилей

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import styles from './navbar.module.css';
+
 const links = [
   { id: 1, title: 'Home', url: '/' },
   { id: 2, title: 'Portfolio', url: '/portfolio' },
