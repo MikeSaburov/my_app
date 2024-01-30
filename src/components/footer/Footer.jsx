d0d0c712f96286180a9eb8685bbf9adc466512f7
Mikhail Saburov <saburov1985@gmail.com>
2024-01-30 23:07:50 +1100

    feature(footer): добавил соцю иконки

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
 import styles from './footer.module.css';
+import Image from 'next/image';
 
 export default function Footer() {
   const date = new Date().getFullYear();
   return (
     <footer className={styles.container}>
       <div>{`${date} MY App. All rights reserved.`}</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/vk.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="vk.com"
+        />
+        <Image
+          src="/inst.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="instagram.com"
+        />
+        <Image
+          src="/twitter.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="twitter.com"
+        />
+        <Image
+          src="/yt.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="youtube.com"
+        />
+      </div>
     </footer>
   );
 }
