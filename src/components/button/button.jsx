953989b741129b012da54e4f7a69a75e18c24c12
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:20:45 +1100

    feature(button): создал компонент кнопки

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,5 +1,10 @@
 import styles from './button.module.css';
+import Link from 'next/link';
 
-export default function Button() {
-  return <div></div>;
+export default function Button({ text, url }) {
+  return (
+    <Link href={url}>
+      <button className={styles.container}>{text}</button>
+    </Link>
+  );
 }
