fdee0e59c42d9cd8c1aaf2ac752abd5dbfc064b1
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:41:43 +1100

    refactor(next.config): поправил путь к сайту

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,12 @@ export const About = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="" fill={true} alt="about us" className="styles.img" />
+        <Image
+          src="https://images.unsplash.com/photo-1563906267088-b029e7101114?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          fill={true}
+          alt="about us"
+          className="styles.img"
+        />
       </div>
     </div>
   );
