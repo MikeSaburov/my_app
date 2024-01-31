6b3a03cfa7d4f82eb11d1d63897b5c9210ab1cfc
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:49:27 +1100

    feature(about): добавил картинку

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ export const About = () => {
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
-          src="https://plus.unsplash.com/premium_photo-1668473365954-c11ca4b7f452?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1548705085-101177834f47?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           fill={true}
           alt="about us"
           className="styles.img"
