690aa0cd4d7fcfd439679cfc8661a5710f35b032
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:12:08 +1100

    refactor(page): стилизовыал параграф

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>The Feature of AI the next few years</h1>
-        <p>
+        <p className={styles.desc}>
           Tirning your Idea into Reality. We brong together the teams from the
           global tech industry.
         </p>
