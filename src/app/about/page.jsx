277014f799f887241ac9825b530915bbff01f709
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:38:16 +1100

    feature(next.config): добавил сайт с картинками

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
 import styles from './about.module.css';
+import Image from 'next/image';
 
 export const About = () => {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="" fill={true} alt="about us" className="styles.img" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
