14563e0ddc006cfc6a1a7333b1a0d5a49aef0c54
Mikhail Saburov <saburov1985@gmail.com>
2024-01-31 22:11:20 +1100

    feature(page): добавил параграф

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,10 @@ export default function Home() {
     <main className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>The Feature of AI the next few years</h1>
+        <p>
+          Tirning your Idea into Reality. We brong together the teams from the
+          global tech industry.
+        </p>
       </div>
     </main>
   );
