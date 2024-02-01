fac5b0354d89112c29b9239714893e78d9872c47
Mikhail Saburov <saburov1985@gmail.com>
2024-02-01 20:58:43 +1100

    refactor(next.config, page): заменил ресурс с картинками и картинку

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ export const About = () => {
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
-          src="https://images.unsplash.com/photo-1548705085-101177834f47?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://img.freepik.com/free-photo/business-team-working-office-worker-concept_53876-23476.jpg?w=1800&t=st=1706781501~exp=1706782101~hmac=14b58d7733fd2bb5fd997c0cfa9bd880e4d0e1758c67384077ca401cc7096770"
           fill={true}
           alt="about us"
           className="styles.img"
