bc07e2522d199bd8b5c0730afbc6de40bc71f685
Mikhail Saburov <saburov1985@gmail.com>
2024-02-01 21:50:14 +1100

    feture(contact): добавил вертску (каринку и заголовок)

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,21 @@
 import styles from './contact.module.css';
+import Image from 'next/image';
+import Button from '@/components/button/button';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contacts"
+          fill={true}
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
