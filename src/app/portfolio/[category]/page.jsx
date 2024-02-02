441591ca7d7c58e3eb748ce8d9ba71a8dcab8b4b
Mikhail Saburov <saburov1985@gmail.com>
2024-02-02 22:47:01 +1100

    refactor(portfolio): выводим категорию на котрую нажали

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,11 @@
 import styles from './category.module.css';
 
 const Category = ({ params }) => {
-  console.log(params);
-  return <div>Category</div>;
+  return (
+    <div>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+    </div>
+  );
 };
 
 export default Category;
