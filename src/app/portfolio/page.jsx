9e190fe24acece138812d63d4e837bf47a1f64aa
Mikhail Saburov <saburov1985@gmail.com>
2024-02-02 22:37:15 +1100

    refactor(portfolio): поменял ссылки на карточках

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,13 +7,13 @@ const Portfolio = () => {
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
         <Link href={'portfolio/illustrations'} className={styles.item}>
-          <span className={styles.title}>illustrations</span>
+          <span className={styles.title}>Illustrations</span>
         </Link>
-        <Link href={'portfolio/illustrations'} className={styles.item}>
-          <span className={styles.title}>illustrations</span>
+        <Link href={'portfolio/websites'} className={styles.item}>
+          <span className={styles.title}>Websites</span>
         </Link>
-        <Link href={'portfolio/illustrations'} className={styles.item}>
-          <span className={styles.title}>illustrations</span>
+        <Link href={'portfolio/application'} className={styles.item}>
+          <span className={styles.title}>Application</span>
         </Link>
       </div>
     </div>
