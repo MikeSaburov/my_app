81a76647020d51338720dd0c6e46a9e52a26d2a8
Mikhail Saburov <saburov1985@gmail.com>
2024-02-02 22:35:20 +1100

    feature(portfolio): добавил карточки

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
 import styles from './portfolio.module.css';
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href={'portfolio/illustrations'} className={styles.item}>
+          <span className={styles.title}>illustrations</span>
+        </Link>
+        <Link href={'portfolio/illustrations'} className={styles.item}>
+          <span className={styles.title}>illustrations</span>
+        </Link>
+        <Link href={'portfolio/illustrations'} className={styles.item}>
+          <span className={styles.title}>illustrations</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
