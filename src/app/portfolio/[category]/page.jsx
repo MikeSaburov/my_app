d2a997b50374a2f4152d4108f413022dda8f600f
Mikhail Saburov <saburov1985@gmail.com>
2024-02-02 22:52:44 +1100

    feature(portfolio/category): набросал верстку

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,25 @@
 import styles from './category.module.css';
-
+import Button from '@/components/button/button';
+import Image from 'next/image';
 const Category = ({ params }) => {
   return (
     <div>
       <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item} key="1">
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1630&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
