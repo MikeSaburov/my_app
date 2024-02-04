b9005f471e20b452621dba299be7d4ce9789ec79
Mikhail Saburov <saburov1985@gmail.com>
2024-02-04 19:42:02 +1100

    refactor(DarkModeToggle): добавил условие для стилей

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,11 +1,15 @@
 import styles from './darkModeToggle.module.css';
 
 export default function DarkModeToggle() {
+  const mode = 'dark';
   return (
     <div className={styles.container}>
       <div className={styles.icon}>🌙</div>
-      <div className={styles.icon}>🔆</div>
-      <div className={styles.ball}></div>
+      <div className={styles.icon}>☀️</div>
+      <div
+        className={styles.ball}
+        style={mode === 'light' ? { left: '2px' } : { right: '2px' }}
+      ></div>
     </div>
   );
 }
