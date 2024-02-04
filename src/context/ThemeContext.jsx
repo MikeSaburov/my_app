d01e3a1853fdc58ffa0fd821c2c48e67145696d4
Mikhail Saburov <saburov1985@gmail.com>
2024-02-04 21:25:10 +1100

    feature(ThemeContext): написал контект для темы

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,1 +1,14 @@
 'use client';
+
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState('dark');
+  const toggle = () => setMode(!mode);
+  return (
+    <ThemeContext.Provider value={{ toggle, mode }}>
+      <div className={`theme ${mode}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
