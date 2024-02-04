c4e84f7390d3de675870a48a199895f391cf4138
Mikhail Saburov <saburov1985@gmail.com>
2024-02-04 22:45:30 +1100

    feature(blog/id): добавлена функция получения данных

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,6 +1,17 @@
 import styles from './id.module.css';
 import Image from 'next/image';
 
+async function getData() {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=5'
+  );
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
 const BlogId = () => {
   return (
     <div className={styles.container}>
