cbc04e14c987502955070cbdc3fff4e60f5c88fa
Mikhail Saburov <saburov1985@gmail.com>
2024-02-05 21:45:24 +1100

    feature(portfolio): динамическое отображение портфолио

--- src/app/portfolio/[category]/data.js
+++ src/app/portfolio/[category]/data.js
@@ -5,21 +5,21 @@ export const items = {
       title: 'Creative Portfolio',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-psd/model-and-actor-portfolio-instagram-stories_23-2149084295.jpg?w=1380&t=st=1707129586~exp=1707130186~hmac=f5d4caa25459a981b3b3614cbe511ba16b25d2a3454a03605484747f8f657299',
     },
     {
       id: 2,
       title: 'Minimal Single Product',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-psd/user-interface-design-instagram-stories_23-2149182788.jpg?w=1380&t=st=1707129715~exp=1707130315~hmac=2a1a5ab3b17aca2e615b507284d1736e9e5723f84e9b9a951acb98223a01e484',
     },
     {
       id: 3,
       title: 'Strong Together Charity',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-vector/minimal-slides-template_52683-2045.jpg?w=1380&t=st=1707129731~exp=1707130331~hmac=3ead00c6d588a0b69e1cfc7975614e4036c0317b6a3c9af72951bac2b1ca209b',
     },
   ],
   illustrations: [
@@ -28,21 +28,21 @@ export const items = {
       title: 'Creative Portfolio',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-vector/asset-selection-concept-illustration_114360-4015.jpg?w=826&t=st=1707129760~exp=1707130360~hmac=a22c72ab66dd562cb8dd8adf2d12100edb67bd0998d5428b945093aef7b19992',
     },
     {
       id: 2,
       title: 'Minimal Single Product',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-vector/content-concept-illustration_114360-2127.jpg?w=826&t=st=1707129777~exp=1707130377~hmac=285de12fa70329425cd56f51596fe8dae12219e27c651bffce8aed581e7595fe',
     },
     {
       id: 3,
       title: 'Strong Together Charity',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-vector/content-concept-illustration_114360-2127.jpg?w=826&t=st=1707129777~exp=1707130377~hmac=285de12fa70329425cd56f51596fe8dae12219e27c651bffce8aed581e7595fe',
     },
   ],
   websites: [
@@ -51,21 +51,21 @@ export const items = {
       title: 'Creative Portfolio',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-psd/gradient-business-template-design_23-2150388910.jpg?w=1380&t=st=1707129822~exp=1707130422~hmac=4adc737b34154e03f77e40238e3bb2df96d92d936b85f43de17724c00bedfea2',
     },
     {
       id: 2,
       title: 'Minimal Single Product',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-psd/halloween-woman-mock-up-on-variety-of-digital-desktop_23-2148313871.jpg?w=1380&t=st=1707129839~exp=1707130439~hmac=0d5d6dd31126cd48bbd4ba8c304aaee780799cf3d79045190eb69aa61d2397ce',
     },
     {
       id: 3,
       title: 'Strong Together Charity',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
       image:
-        'https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b',
+        'https://img.freepik.com/free-psd/business-strategy-landing-page-template_23-2150027245.jpg?w=1380&t=st=1707129869~exp=1707130469~hmac=32e5625cece92a11a9f2b950a07d590b7f35548f4ba5731c26b9d0bc48cef41e',
     },
   ],
 };
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,50 +1,39 @@
 import styles from './category.module.css';
 import Button from '@/components/button/button';
 import Image from 'next/image';
+import { items } from './data';
+import { notFound } from 'next/navigation';
+
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
+
 const Category = ({ params }) => {
+  const data = getData(params.category);
   return (
     <div>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item} key="1">
-        <div className={styles.content}>
-          <h1 className={styles.title}>Lorem</h1>
-          <p className={styles.desc}>
-            Ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            impedit sequi ab. Optio impedit consectetur obcaecati, quasi ab
-            reiciendis libero magni dolorum quisquam! Autem, excepturi officia
-            perspiciatis nulla laudantium ad?
-          </p>
-          <Button text="See More" url="#" />
+      {data.map((cat) => (
+        <div className={styles.item} key={cat.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{cat.title}</h1>
+            <p className={styles.desc}>{cat.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={cat.image}
+              alt={cat.title}
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://img.freepik.com/free-vector/shed-concept-illustration_114360-8308.jpg?w=826&t=st=1706951334~exp=1706951934~hmac=30850bf01c30e939de3db8b9a862559a80c5c8044a8f97d5ee8ead608d635a62"
-            alt="image"
-          />
-        </div>
-      </div>
-      <div className={styles.item} key="2">
-        <div className={styles.content}>
-          <h1 className={styles.title}>Lorem</h1>
-          <p className={styles.desc}>
-            Ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            impedit sequi ab. Optio impedit consectetur obcaecati, quasi ab
-            reiciendis libero magni dolorum quisquam! Autem, excepturi officia
-            perspiciatis nulla laudantium ad?
-          </p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://img.freepik.com/free-vector/select-house-concept-illustration_114360-4098.jpg?w=1380&t=st=1706951590~exp=1706952190~hmac=c938eb0cda793ede3701184cbf8464dbba682050f00ca116141b85b68f8df1cd"
-            alt="image"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
