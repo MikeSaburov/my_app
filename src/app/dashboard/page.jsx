9ef17ce167008fcbcc973e6329d4c0078dfe2675
Mikhail Saburov <saburov1985@gmail.com>
2024-02-05 21:23:29 +1100

    refactor(dashboard): получаем анные с помощью swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,31 +1,40 @@
 'use client';
 import { useState, useEffect } from 'react';
 import styles from './dashboard.module.css';
+import useSWR from 'swr';
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch(
-        'https://jsonplaceholder.typicode.com/posts?_limit=15',
-        { cache: 'no-store' }
-      );
-      if (!res.ok) {
-        setError(true);
-      }
-      const data = await res.json();
-      setData(data);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await fetch(
+  //       'https://jsonplaceholder.typicode.com/posts?_limit=15',
+  //       { cache: 'no-store' }
+  //     );
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
 
+  // console.log(data);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts?_limit=15',
+    fetcher
+  );
+  if (error) return <div>ошибка загрузки</div>;
+  if (isLoading) return <div>загрузка...</div>;
   console.log(data);
-
-  return <div>Dashboard</div>;
+  return <div>DAshboard</div>;
 };
 
 export default Dashboard;
