a3abfff087b263fdcd39126f1515c78324d6d6d4
Mikhail Saburov <saburov1985@gmail.com>
2024-02-05 20:42:19 +1100

    refactor(dashboard): импортировал хуки useState и useEffect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,12 @@
+'use client';
+import { useState, useEffect } from 'react';
 import styles from './dashboard.module.css';
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   return <div>Dashboard</div>;
 };
 
