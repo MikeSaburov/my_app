51ed7794c5122be5f22a8b1f928220ce9ef23090
Mikhail Saburov <saburov1985@gmail.com>
2024-02-05 20:53:06 +1100

    refactor(dashboard): написана функция получения данных

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,7 +5,25 @@ import styles from './dashboard.module.css';
 const Dashboard = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(
+        'https://jsonplaceholder.typicode.com/posts?_limit=15',
+        { cache: 'no-store' }
+      );
+      if (!res.ok) {
+        setError(true);
+      }
+      const data = await res.json();
+      setData(data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  console.log(data);
 
   return <div>Dashboard</div>;
 };
