3e536a93879ee85c204c5a9e874095d987a44071
Mikhail Saburov <saburov1985@gmail.com>
2024-02-07 21:03:09 +1100

    feature(Post): создана схема для поста

---
+++ src/models/Post.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const { Shema } = mongoose;
+
+const postSchema = new Shema(
+  {
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    desc: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    img: {
+      type: String,
+    },
+    content: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Post', postSchema);
