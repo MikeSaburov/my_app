9705f526c86cc88d9e2e42ef93cd99053d0d580e
Mikhail Saburov <saburov1985@gmail.com>
2024-02-08 22:40:51 +1100

    feature(dashboard/login): сверстал страницу входа

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,27 @@
 import styles from './login.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
+
+      <button>Login with Google</button>
+    </div>
+  );
 };
 
 export default Login;
