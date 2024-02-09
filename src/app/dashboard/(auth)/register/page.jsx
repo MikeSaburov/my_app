3d4213abe5a87334d793255f9d2eb63f0762ff87
Mikhail Saburov <saburov1985@gmail.com>
2024-02-09 22:12:38 +1100

    refactor(auth/register): сверстана страница регистрации

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,6 +1,40 @@
+'use client';
+
+import Link from 'next/link';
 import styles from './register.module.css';
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">
+        Login with an existing account
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
