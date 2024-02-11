da6ffcf1cab01cda0830be8183c44084a11e0e29
Mikhail Saburov <saburov1985@gmail.com>
2024-02-11 23:07:01 +1100

    feature(auth/register): создал роутер

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,14 +1,29 @@
 import User from '@/models/User';
 import connect from '@/utils/db';
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
 
 export const POST = async (req) => {
   const { name, email, password } = req.json();
 
   await connect();
 
+  const hashedPassword = await bcrypt.hash(password, 7);
+
   const newUser = new User({
     name,
     email,
     password: hashedPassword,
   });
+
+  try {
+    await newUser.save();
+    return new NextResponse('Пользователь успешно создан', {
+      status: 201,
+    });
+  } catch (error) {
+    return new NextResponse('Пользователь не создан' + error.message, {
+      status: 500,
+    });
+  }
 };
