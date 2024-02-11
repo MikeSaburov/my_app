988a468624306ec2f181bd52fca48b86e821b676
Mikhail Saburov <saburov1985@gmail.com>
2024-02-11 22:57:56 +1100

    feature(package): install bcrypt

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,7 +1,14 @@
+import User from '@/models/User';
 import connect from '@/utils/db';
 
 export const POST = async (req) => {
   const { name, email, password } = req.json();
 
   await connect();
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
 };
