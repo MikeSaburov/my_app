be552cc01fea327b86f968e2b9ed91d0e60dcc30
Mikhail Saburov <saburov1985@gmail.com>
2024-02-14 21:00:06 +1100

    feature(api/posts): получаем только посты пользователя

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,11 +1,15 @@
 import Post from '@/models/Post';
 import connect from '@/utils/db';
+import { request } from 'express';
 import { NextResponse } from 'next/server';
 
 export const GET = async (req) => {
+  const url = new URL(request.url);
+  const username = url.searchParams.get('username');
+
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { uername });
     return new NextResponse(JSON.stringify(posts), { status: 200 });
   } catch (error) {
     return new NextResponse('Ошибка ответа БД!', { status: 500 });
