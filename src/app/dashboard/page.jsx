30680c1175bb15f1dce17a2dbf7a658fa56b419c
Mikhail Saburov <saburov1985@gmail.com>
2024-02-14 20:49:11 +1100

    refactor(dashboard): перенапраление если не зашел
    перенапраление пользователя на страницу входа если он не зашел и хочет попать на страницу dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import styles from './dashboard.module.css';
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -28,7 +29,7 @@ const Dashboard = () => {
   // console.log(data);
 
   const session = useSession();
-  console.log(session);
+  const router = useRouter();
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
@@ -37,6 +38,10 @@ const Dashboard = () => {
   );
   if (error) return <div>Ошибка загрузки</div>;
   if (isLoading) return <div>Загрузка...</div>;
+
+  if (session.status == 'unauthenticated') {
+    router?.push('/dashboard/login');
+  }
 
   return <div>Dashboard</div>;
 };
