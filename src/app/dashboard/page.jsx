9c1f9a99aae3410d000092d3caaa5569df5640f5
Mikhail Saburov <saburov1985@gmail.com>
2024-02-17 21:31:48 +1100

    refactor(dashboard): удалил закомментированный код

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -8,29 +8,6 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 const Dashboard = () => {
-  // const [data, setData] = useState([]);
-  // const [error, setError] = useState(false);
-  // const [isLoading, setIsLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-  //       cache: "no-store",
-  //     });
-
-  //     if (!res.ok) {
-  //       setError(true);
-  //     }
-  //     const data = await res.json()
-
-  //     setData(data)
-  //     setIsLoading(false)
-  //   };
-  //   getData()
-  // },[]);
-
-  // console.log(data);
-
   const session = useSession();
   const router = useRouter();
 
