666e1ca020e8b146733bd31828bc51b84bce3ef5
Mikhail Saburov <saburov1985@gmail.com>
2024-02-18 23:01:11 +1100

    refactor(blog): изменил ссылку

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 };
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts', {
+  const res = await fetch('/api/posts', {
     cache: 'no-store',
   });
   if (!res.ok) {
